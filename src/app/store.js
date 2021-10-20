bda7b426ef00fda536e21e316d00cdafc8bf616b
Eduard Gavrila <edygavrila@gmail.com>
2021-10-20 23:02:19 +0100

    created the folder structure

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/gameState/gameStateSlice";
 
 export const store = configureStore({
   reducer: {
